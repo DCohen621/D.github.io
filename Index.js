7e8661cc170f42d82560beaa553ff9afbab4e89c
Dylan <dylanreid621@gmail.com>
2024-07-31 11:46:12 -0500

    jhfcjgcvm

---
+++ Index.js
@@ -0,0 +1,12 @@
+let result = document.getElementById("GFG");
+
+    function changeColor(color) {
+        document.body.style.background = color;
+    }
+
+    function myFunc() {
+    changeColor('white');
+    }
+    function myFunc2() {
+        changeColor('black');
+    }
