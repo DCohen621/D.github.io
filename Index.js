cf63c593b5f285009162a345bdee74eb5d834a5e
Dylan <dylanreid621@gmail.com>
2024-08-01 11:09:23 -0500

    dfjdghjhkjhgfd

--- Index.js
+++ Index.js
@@ -10,3 +10,12 @@ let result = document.getElementById("GFG");
     function myFunc2() {
         changeColor('black');
     }
+    
+    function show() {
+        /* Access image by id and change 
+        the display property to block*/
+        document.getElementById('image')
+            .style.display = "block";
+        document.getElementById('btnID')
+            .style.display = "none";
+    }
